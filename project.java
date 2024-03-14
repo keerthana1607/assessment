198209be91f9de68656eb855f1d3411ba62e1bf7
Meenakshi <meenakshiananthan8@gmail.com>
2024-03-14 11:54:51 +0530

    Meenakshi updated

--- project.java
+++ project.java
@@ -1,6 +1,16 @@
 class project{
-  public static void main(String[] args){
-    System.out.println("Group assessment");
+  
+    public int Multiplication(int a, int b){
+	    int product = a*b;
+	    return product;
   }
+  public static void main(String args[]){
+	  System.out.println("Group assessment");
+	  int a = 10;
+	  int b = 20;
+
+	  project pr = new project();
+	  int result = pr.Multiplication(a, b);
+	  System.out.println("Product is: " + result);
 }
     
