5dc0a443b1dfe33014854905c45d34cf33375255
Madhan <madhans28803@gmail.com>
2024-03-14 11:54:02 +0530

    edited by madhan

--- project.java
+++ project.java
@@ -1,5 +1,14 @@
 class project{
+	public int addNumber(int a,int b) {
+		int sum = a + b;
+		return sum;
+	}
   public static void main(String[] args){
+	  int num1 = 10;
+	  int num2 = 20;
+	  project obj = new project();
+	  int result = obj.addNumber(num1,num2)
+	  System.out.println("sum is:" +result) ;
     System.out.println("Group assessment");
   }
 }
