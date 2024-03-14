d855bae15c902b8b4f7fbbf75b41c005cc93ddc4
kiruthikka <kiruthikkamuthuraman@gmail.com>
2024-03-14 11:56:15 +0530

    kiruthikka updated

--- project.java
+++ project.java
@@ -1,5 +1,16 @@
 class project{
-  public static void main(String[] args){
+	  public int SubNumbers(int a , int b){
+		  int diff = a - b:
+			  //return value 
+			  return diff;
+		  public static void main(String[]args){
+			  int num1 = 25;
+			  int num2 = 15;
+			  //Create an object of main
+			  Main obj = new Main();
+			  //Calling method
+			  int result = obj.diffNumbers(num1,num2);
+			  System.out.println("Sum is :" + result);
     System.out.println("Group assessment");
   }
 }
